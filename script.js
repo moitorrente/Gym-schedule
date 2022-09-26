59ae29425acb006c919ef969aea7aa3f7442da1d
moitorrente <moi.torrente@gmail.com>
2022-09-26 04:30:39 +0200

    Mejora UI

--- script.js
+++ script.js
@@ -15,25 +15,36 @@ function loadExercises() {
             const checked = ejercicio.aitor || ejercicio.moi ? 'checked' : '';
             createNewExercise(ejercicio.orden, ejercicio.nombre, ejercicio.series, ejercicio.id, checked)
         });
+    }else{
+        const a = document.createElement('a');
+        a.innerHTML = 'Añadir ejercicio';
+        a.classList.add('text-center', 'my-4', 'py-4')
+        a.href = 'add-exercise.html'
+        listaEjercicios.appendChild(a)
     }
 }
 
 function createNewExercise(order, name, series, id, checked) {
     const d = document.createElement('div');
+    d.classList.add('py-1')
     d.innerHTML = `
-    <a class="list-group-item d-flex gap-3 rounded" data-id="${id}" id="start-${id}">
-        <input class="form-check-input flex-shrink-0" type="checkbox" ${checked} disabled value="" style="font-size: 1.375em;">
+    <a class="list-group-item d-flex gap-3 p-2 rounded" data-id="${id}" id="start-${id}">
+        <input class="my-0 form-check-input flex-shrink-0 align-self-center" type="checkbox" ${checked} disabled value="" style="font-size: 1.375em;">
+        <span class="flex-shrink-0 align-self-center">${order}</span>
         <span class="pt-1 form-checked-content">
-            <strong>${order}</strong>
             <strong>${name}</strong>
             <small class="d-block text-muted">Series: ${series}</small>
+        </span>
+        <span class="align-self-center ms-auto">
+            <svg  xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+            </svg>
         </span>
     </a>
     `;
 
     listaEjercicios.appendChild(d);
     const start = document.getElementById(`start-${id}`);
-    console.log(start)
 
     start.onclick = () =>{
         localStorage.setItem('current', start.getAttribute('data-id'));
@@ -48,7 +59,7 @@ share.onclick = () => {
     const ejercicios = JSON.parse(localStorage.getItem('ejercicios'));
     const text = ejercicios.map(ejercicio => json2csv(ejercicio)).join('');
 
-    share.href =  `https://wa.me?text=${encodeURIComponent(text)}`
+    share.href = `https://wa.me?text=${encodeURIComponent(text)}`
 }
 
 function json2csv(ob){
@@ -69,3 +80,4 @@ function json2csv(ob){
 
     return aitor + '\r\n' + moi + '\r\n';           
 }
+
