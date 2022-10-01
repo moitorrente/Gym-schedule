c88e8d8af82c213916feec395323af2a5449ed56
moitorrente <moi.torrente@gmail.com>
2022-10-01 20:10:59 +0200

    PWA update

--- sw.js
+++ sw.js
@@ -1,58 +1,57 @@
-var APP_PREFIX = 'GymSchedule_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'v0.0.6'              // Version of the off-line cache (change this value everytime you want to update cache)
-var CACHE_NAME = APP_PREFIX + VERSION
-var URLS = [                            // Add URL you want to cache in this list.
-  '/Gym-schedule/',                     // If you have separate JS/CSS files,
-  '/Gym-schedule/index.html'            // add path to those files here
-]
 
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
 
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
-    })
-  )
-})
+const GHPATH = '/Gym-schedule';
+const APP_PREFIX = 'Gym_schedule_';
+const VERSION = 'version_006';
 
-// Cache resources
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(URLS)
-    })
-  )
-})
+const URLS = [    
+    `${GHPATH}/`,
+    `${GHPATH}/index.html`,
+    `${GHPATH}/common.css`,
+    `${GHPATH}/script.js`,
+    `${GHPATH}/style.css`,
+    `${GHPATH}/calendar.html`
 
-// Delete outdated caches
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create white list
-      var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX)
-      })
-      // add current cache name to white list
-      cacheWhitelist.push(CACHE_NAME)
+  ]
 
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] )
-          return caches.delete(keyList[i])
+  var CACHE_NAME = APP_PREFIX + VERSION
+  self.addEventListener('fetch', function (e) {
+    console.log('Fetch request : ' + e.request.url);
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { 
+          console.log('Responding with cache : ' + e.request.url);
+          return request
+        } else {       
+          console.log('File is not cached, fetching : ' + e.request.url);
+          return fetch(e.request)
         }
-      }))
-    })
-  )
-})
+      })
+    )
+  })
+  
+  self.addEventListener('install', function (e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('Installing cache : ' + CACHE_NAME);
+        return cache.addAll(URLS)
+      })
+    )
+  })
+  
+  self.addEventListener('activate', function (e) {
+    e.waitUntil(
+      caches.keys().then(function (keyList) {
+        var cacheWhitelist = keyList.filter(function (key) {
+          return key.indexOf(APP_PREFIX)
+        })
+        cacheWhitelist.push(CACHE_NAME);
+        return Promise.all(keyList.map(function (key, i) {
+          if (cacheWhitelist.indexOf(key) === -1) {
+            console.log('Deleting cache : ' + keyList[i] );
+            return caches.delete(keyList[i])
+          }
+        }))
+      })
+    )
+  })
