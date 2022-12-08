da534221b836efeb24ef938481089cf23bafed73
moitorrente <moi.torrente@gmail.com>
2022-12-08 01:04:58 +0100

    Actualización versión PWA

--- js/settings.js
+++ js/settings.js
@@ -1,9 +1,11 @@
-const forceReload = () =>
+const forceReload = () => {
     navigator.serviceWorker
         .getRegistrations()
         .then((registrations) =>
             Promise.all(registrations.map((r) => r.unregister())),
         )
         .then(() => window.location.reload());
+}
+
 
 document.getElementById('force-update').onclick = () => forceReload();
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const GHPATH = '/Gym-schedule';
 const APP_PREFIX = 'Gym_schedule_';
-const VERSION = 'version_0012r';
+const VERSION = 'version_0012s';
 
 const URLS = [
   `${GHPATH}/`,
