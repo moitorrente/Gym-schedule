a363a2f0ad247af6ef8619ba0a58c720ff60f745
moitorrente <moi.torrente@gmail.com>
2022-12-10 14:19:25 +0100

    Asignación de versión

--- js/settings.js
+++ js/settings.js
@@ -1,16 +1,8 @@
 import getFile from './data.js';
-checkUpdate();
-async function checkUpdate() {
+getVersion();
+async function getVersion() {
     const localVersion = await getFile('version.json');
-    const githubVersion = await getgit('moitorrente', 'Gym-schedule', 'data/version.json');
-
-    console.log(`Versión caché: ${localVersion.data.version} | Versión github: ${githubVersion.version}`)
-
-    if (localVersion.data.version != githubVersion.version) {
-        document.getElementById('update-pending').classList.remove('d-none')
-    } else {
-        document.getElementById('update-pending').classList.add('d-none')
-    }
+    document.getElementById('version-text').innerHTML = localVersion.data.version;
 }
 
 async function forceReload() {
@@ -26,20 +18,3 @@ async function forceReload() {
 }
 
 document.getElementById('force-update').onclick = () => forceReload();
-
-async function getgit(owner, repo, path) {
-    // A function to fetch files from github using the api 
-
-    let data = await fetch(
-        `https://api.github.com/repos/${owner}/${repo}/contents/${path}`
-    )
-        .then(d => d.json())
-        .then(d =>
-            fetch(
-                `https://api.github.com/repos/${owner}/${repo}/git/blobs/${d.sha}`
-            )
-        )
-        .then(d => d.json())
-        .then(d => JSON.parse(atob(d.content)));
-    return data;
-}
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const GHPATH = '/Gym-schedule';
 const APP_PREFIX = 'Gym_schedule_';
-const VERSION = 'version_0013h';
+const VERSION = 'version_0013g';
 
 const URLS = [
   `${GHPATH}/`,
