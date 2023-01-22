f9c6e5f4150224c0f7523b396407c9e9a8ec8eb5
moitorrente <moi.torrente@gmail.com>
2023-01-22 17:41:00 +0100

    Actualización sw

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 const GHPATH = '/Gym-schedule';
 const APP_PREFIX = 'Gym_schedule_';
 const VERSION = 'version_0015f';
+const RUNTIME = 'runtime';
 
 const URLS = [
   `${GHPATH}/`,
@@ -43,62 +44,75 @@ const URLS = [
   `${GHPATH}/data/workouts.json`
 ]
 
-var CACHE_NAME = APP_PREFIX + VERSION;
-self.addEventListener('fetch', function (e) {
-  console.log('Fetch request : ' + e.request.url);
-  e.respondWith(
-    caches.match(e.requests, { ignoreSearch: true }).then(function (request) {
-      if (request) {
-        console.log('Responding with cache : ' + e.request.url);
-        return request
-      } else {
-        console.log('File is not cached, fetching : ' + e.request.url);
-        return fetch(e.request)
-      }
-    })
-  )
-})
-
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('Installing cache : ' + CACHE_NAME);
-      return cache.addAll(URLS)
-    })
-  )
-})
-
-// self.addEventListener('activate', function (e) {
-//   e.waitUntil(
-//     caches.keys().then(function (keyList) {
-//       var cacheWhitelist = keyList.filter(function (key) {
-//         return key.indexOf(APP_PREFIX)
-//       })
-//       cacheWhitelist.push(CACHE_NAME);
-//       return Promise.all(keyList.map(function (key, i) {
-//         if (cacheWhitelist.indexOf(key) === -1) {
-//           console.log('Deleting cache : ' + keyList[i]);
-//           return caches.delete(keyList[i])
-//         }
-//       }))
+const CACHE_NAME = APP_PREFIX + VERSION;
+// self.addEventListener('fetch', function (e) {
+//   console.log('Fetch request : ' + e.request.url);
+//   e.respondWith(
+//     caches.match(e.requests).then(function (request) {
+//       if (request) {
+//         console.log('Responding with cache : ' + e.request.url);
+//         return request
+//       } else {
+//         console.log('File is not cached, fetching : ' + e.request.url);
+//         return fetch(e.request)
+//       }
 //     })
 //   )
 // })
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache); //Deleting the old cache (cache v1)
-          }
-        })
-      );
-    })
-      .then(function () {
-        console.info("Old caches are cleared!");
-        return self.clients.claim();
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          console.log('Responding with cache : ' + cachedResponse.request.url);
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
       })
+    );
+  }
+});
+
+// self.addEventListener('install', function (e) {
+//   e.waitUntil(
+//     caches.open(CACHE_NAME).then(function (cache) {
+//       console.log('Installing cache : ' + CACHE_NAME);
+//       return cache.addAll(URLS)
+//     })
+//   )
+// })
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(URLS))
+      .then(self.skipWaiting())
   );
 });
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      cacheWhitelist.push(CACHE_NAME);
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('Deleting cache : ' + keyList[i]);
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
