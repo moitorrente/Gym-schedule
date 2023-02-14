8d70f13c574f921ad0b74bc7b55a51c3fb1914e9
moitorrente <moi.torrente@gmail.com>
2023-02-14 16:44:00 +0100

    Añadidos diferentes fondos

--- js/user.js
+++ js/user.js
@@ -2,17 +2,36 @@ const user = document.getElementById('user');
 const height = document.getElementById('height');
 
 const saveBtn = document.getElementById('save');
+let bg = 'wavy';
 saveBtn.onclick = () => {
-
+    // if (!img) {
+    //     const saved = localStorage.user;
+    //     if (saved) {
+    //         img = JSON.parse(saved).image;
+    //     } else {
+    //         img = `../icon.png`;
+    //     }
+    // }
 
     const userToSave = {
         name: user.value,
         height: height.value,
-        image: img
+        background: bg
+        // image: img
     }
     localStorage.setItem('user', JSON.stringify(userToSave));
     getContext();
 }
+
+const bgSelector = document.querySelectorAll('.bg-selector');
+bgSelector.forEach(x => x.onclick = () => {
+    bg = x.dataset.bg;
+    document.getElementById('user-card-container').classList = null;
+    document.getElementById('user-card-container').classList.add(bg);
+
+})
+
+
 
 getContext();
 function getContext() {
@@ -23,27 +42,29 @@ function getContext() {
         height.value = userStorage.height;
         document.getElementById('user-display').innerHTML = userStorage.name;
         document.getElementById('height-display').innerHTML = userStorage.height + ' cm';
-        document.getElementById('profile-picture-display').src = userStorage.image;
+        document.getElementById('user-card-container').classList = null;
+
+        document.getElementById('user-card-container').classList.add(userStorage.background);
+    } else {
+        document.getElementById('user-card-container').classList = null;
+
+        document.getElementById('user-card-container').classList.add('wavy');
+
     }
 }
 
 
-const profilePicture = document.getElementById("profilePicture");
-let img;
-profilePicture.addEventListener("change", () => {
-    const image = profilePicture.files[0];
-    const reader = new FileReader();
+// const profilePicture = document.getElementById("profilePicture");
+// let img;
+// profilePicture.addEventListener("change", () => {
+//     const image = profilePicture.files[0];
+//     const reader = new FileReader();
 
-    reader.readAsDataURL(image);
-    reader.addEventListener('load', () => {
-        img = reader.result;
-        // let user = localStorage.user;
-        // if (user) {
-        //     user = JSON.parse(user);
-        //     user.image = reader.result;
-        //     localStorage.setItem('user', JSON.stringify(user));
-        // }
-    });
+//     reader.readAsDataURL(image);
+//     reader.addEventListener('load', () => {
+//         img = reader.result;
+
+//     });
 
 
-})
+// })
