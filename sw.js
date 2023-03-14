1c8e1e64079ed92a0bc555db642315b71395498b
moitorrente <moi.torrente@gmail.com>
2023-03-14 18:47:08 +0100

    update version

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const GHPATH = '/Gym-schedule';
 const APP_PREFIX = 'Gym_schedule_';
-const VERSION = 'version_0016c';
+const VERSION = 'version_0016d';
 const RUNTIME = 'runtime';
 
 const URLS = [
